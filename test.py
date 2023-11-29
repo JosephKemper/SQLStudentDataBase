3935fb268d9161cf071211ad306c42d83f7b4157
Joseph Kemper <JosephKemper@gmail.com>
2023-11-28 19:30:40 -0700

    Added both individual names and many names into db

--- test.py
+++ test.py
@@ -11,12 +11,36 @@ my_cursor = connection.cursor()
 
 # Create a table
 # Triple quotes lets the table entry be completed over multiple lines. 
-my_cursor.execute("""CREATE TABLE customers (
-                  first_name text,
-                  last_name text,
-                  email text
-                  )
-""")
+#my_cursor.execute("""CREATE TABLE customers (
+#                  first_name text,
+#                  last_name text,
+#                  email text
+#                  )
+#""")
+
+# For inserting one name into a database
+# SQL commands are always in upper case
+# my_cursor.execute("INSERT INTO customers VALUES ('John', 'Elder', 'john@codemy.com')")
+# my_cursor.execute("INSERT INTO customers VALUES ('Joseph', 'Kemper', 'JosephKemper@gmail.com')")
+
+# List of many names
+many_customers = [
+    ('John', 'Doe', 'john.doe@example.com'),
+    ('Jane', 'Doe', 'jane.doe@example.com'),
+    ('Jim', 'Brown', 'jim.brown@example.com'),
+    ('Jill', 'Smith', 'jill.smith@example.com'),
+    ('Jack', 'Johnson', 'jack.johnson@example.com'),
+    ('Julia', 'Davis', 'julia.davis@example.com'),
+    ('Jerry', 'Miller', 'jerry.miller@example.com'),
+    ('Jasmine', 'Wilson', 'jasmine.wilson@example.com'),
+    ('Jake', 'Moore', 'jake.moore@example.com'),
+    ('Joyce', 'Taylor', 'joyce.taylor@example.com'),
+    ('Joe', 'Anderson', 'joe.anderson@example.com'),
+    ('Jenny', 'Thomas', 'jenny.thomas@example.com')
+]
+
+# To add in many names into a customer list
+my_cursor.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
 
 # SQLite data types
 # NULL - Does it exist or not
