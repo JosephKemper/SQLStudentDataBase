23240e456080651267fecdf3001b1eb0b13b7916
Joseph Kemper <JosephKemper@gmail.com>
2023-11-28 19:13:37 -0700

    Created first table

--- test.py
+++ test.py
@@ -6,3 +6,28 @@ import sqlite3
 # To store for permanent use
 connection = sqlite3.connect("customer.db")
 
+# Create a cursor
+my_cursor = connection.cursor()
+
+# Create a table
+# Triple quotes lets the table entry be completed over multiple lines. 
+my_cursor.execute("""CREATE TABLE customers (
+                  first_name text,
+                  last_name text,
+                  email text
+                  )
+""")
+
+# SQLite data types
+# NULL - Does it exist or not
+# INTEGER - number
+# REAL - decimal
+# TEXT - text string
+# BLOB - everything else
+
+# Commit our command
+connection.commit()
+
+# Close our connection #Best practice
+connection.close()
+
