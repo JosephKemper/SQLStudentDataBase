96999f862b0f404cf97e1ea6c2902d7973d5d244
Joseph Kemper <JosephKemper@gmail.com>
2023-11-28 19:44:49 -0700

    Used Fetch and print to display data to screen.

--- test.py
+++ test.py
@@ -24,23 +24,34 @@ my_cursor = connection.cursor()
 # my_cursor.execute("INSERT INTO customers VALUES ('Joseph', 'Kemper', 'JosephKemper@gmail.com')")
 
 # List of many names
-many_customers = [
-    ('John', 'Doe', 'john.doe@example.com'),
-    ('Jane', 'Doe', 'jane.doe@example.com'),
-    ('Jim', 'Brown', 'jim.brown@example.com'),
-    ('Jill', 'Smith', 'jill.smith@example.com'),
-    ('Jack', 'Johnson', 'jack.johnson@example.com'),
-    ('Julia', 'Davis', 'julia.davis@example.com'),
-    ('Jerry', 'Miller', 'jerry.miller@example.com'),
-    ('Jasmine', 'Wilson', 'jasmine.wilson@example.com'),
-    ('Jake', 'Moore', 'jake.moore@example.com'),
-    ('Joyce', 'Taylor', 'joyce.taylor@example.com'),
-    ('Joe', 'Anderson', 'joe.anderson@example.com'),
-    ('Jenny', 'Thomas', 'jenny.thomas@example.com')
-]
+# many_customers = [
+#     ('John', 'Doe', 'john.doe@example.com'),
+#     ('Jane', 'Doe', 'jane.doe@example.com'),
+#     ('Jim', 'Brown', 'jim.brown@example.com'),
+#     ('Jill', 'Smith', 'jill.smith@example.com'),
+#     ('Jack', 'Johnson', 'jack.johnson@example.com'),
+#     ('Julia', 'Davis', 'julia.davis@example.com'),
+#     ('Jerry', 'Miller', 'jerry.miller@example.com'),
+#     ('Jasmine', 'Wilson', 'jasmine.wilson@example.com'),
+#     ('Jake', 'Moore', 'jake.moore@example.com'),
+#     ('Joyce', 'Taylor', 'joyce.taylor@example.com'),
+#     ('Joe', 'Anderson', 'joe.anderson@example.com'),
+#     ('Jenny', 'Thomas', 'jenny.thomas@example.com')
+# ]
 
 # To add in many names into a customer list
-my_cursor.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+# my_cursor.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+
+# Query the database
+my_cursor.execute("SELECT * FROM customers")
+# my_cursor.fetchone() # Get one from DB
+# my_cursor.fetchmany(3) # Get 3 from DB Gets the first 3 entered into the list
+customers = my_cursor.fetchall() # Get everything from DB 
+# Data can is stored in a tuple inside of a list. 
+# Data can be accessed in the same way a list/tuple can
+
+for customer in customers:
+    print(customer[0] + " " + customer[1] + "\t" + customer[2])
 
 # SQLite data types
 # NULL - Does it exist or not
