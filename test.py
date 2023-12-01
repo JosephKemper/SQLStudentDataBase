8d0928157b9f09fdd3956108763bb9044010c58c
Joseph Kemper <JosephKemper@gmail.com>
2023-11-30 19:34:09 -0700

    Started building SQL app.

--- test.py
+++ test.py
@@ -1,69 +1,39 @@
 import sqlite3
 
-# To store in memory for one time use
-#connection = sqlite3.connect(":memory:")
 
-# To store for permanent use
-connection = sqlite3.connect("customer.db")
+def connect_database():
+    # Connect to database:
+    connection = sqlite3.connect('customer.db')
+    # Create a cursor
+    cursor = connection.cursor()
+    return connection, cursor
 
-# Create a cursor
-my_cursor = connection.cursor()
+# Commit any changes and close connection to database
+def close_database(connection):
+    connection.commit()
+    connection.close()
 
-# Create a table
-# Triple quotes lets the table entry be completed over multiple lines. 
-#my_cursor.execute("""CREATE TABLE customers (
-#                  first_name text,
-#                  last_name text,
-#                  email text
-#                  )
-#""")
+# Query database and show all records
+def show_all():
+    connection, cursor = connect_database()
 
-# For inserting one name into a database
-# SQL commands are always in upper case
-# my_cursor.execute("INSERT INTO customers VALUES ('John', 'Elder', 'john@codemy.com')")
-# my_cursor.execute("INSERT INTO customers VALUES ('Joseph', 'Kemper', 'JosephKemper@gmail.com')")
+    # Query Data base
+    cursor.execute("SELECT rowid, * FROM customers")
+    customers = cursor.fetchall()
 
-# List of many names
-# many_customers = [
-#     ('John', 'Doe', 'john.doe@example.com'),
-#     ('Jane', 'Doe', 'jane.doe@example.com'),
-#     ('Jim', 'Brown', 'jim.brown@example.com'),
-#     ('Jill', 'Smith', 'jill.smith@example.com'),
-#     ('Jack', 'Johnson', 'jack.johnson@example.com'),
-#     ('Julia', 'Davis', 'julia.davis@example.com'),
-#     ('Jerry', 'Miller', 'jerry.miller@example.com'),
-#     ('Jasmine', 'Wilson', 'jasmine.wilson@example.com'),
-#     ('Jake', 'Moore', 'jake.moore@example.com'),
-#     ('Joyce', 'Taylor', 'joyce.taylor@example.com'),
-#     ('Joe', 'Anderson', 'joe.anderson@example.com'),
-#     ('Jenny', 'Thomas', 'jenny.thomas@example.com')
-# ]
+    for customer in customers:
+        print(customer)
 
-# To add in many names into a customer list
-# my_cursor.executemany("INSERT INTO customers VALUES (?,?,?)", many_customers)
+    close_database(connection)
 
-# Query the database
-my_cursor.execute("SELECT rowid, * FROM customers")
-# my_cursor.fetchone() # Get one from DB
-# my_cursor.fetchmany(3) # Get 3 from DB Gets the first 3 entered into the list
-customers = my_cursor.fetchall() # Get everything from DB 
-# Data can is stored in a tuple inside of a list. 
-# Data can be accessed in the same way a list/tuple can
 
-for customer in customers:
-    print (f"{customer[0]} {customer[1]} {customer [2]} {customer[3]}")
-#    print(customer)
+# Add a single record to the database
+def add_record(first, last, email):
+    connection, cursor = connect_database()
 
-# SQLite data types
-# NULL - Does it exist or not
-# INTEGER - number
-# REAL - decimal
-# TEXT - text string
-# BLOB - everything else
+    cursor.execute("INSERT INTO customers VALUES (?, ?, ?)", (first, last, email))
 
-# Commit our command
-connection.commit()
+    close_database(connection)
 
-# Close our connection #Best practice
-connection.close()
 
+# Delete a single record from the database
---
+++ test1.py
@@ -0,0 +1,5 @@
+import test
+
+test.add_record("Laura", "Smith", "laura@smith.com")
+
+test.show_all()
