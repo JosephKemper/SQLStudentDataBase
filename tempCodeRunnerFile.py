0282739926b703df2ea4dac9eb40d94795a00167
Joseph Kemper <JosephKemper@gmail.com>
2023-12-01 20:24:55 -0700

    Finished tutorial with a few additions

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+customer_list = [
+    ("kdjfsgh","m,fhbg","nvkghdfg"),
+    ("ytvdsdf","ncjfgh","yr87gbf")
+    ]
+test.add_customer_list(customer_list)
--- test1.py
+++ test1.py
@@ -1,5 +1,17 @@
 import test
 
-test.add_record("Laura", "Smith", "laura@smith.com")
+#test.add_customer("Jason", "Smith", "jason@smith.com")
 
-test.show_all()
+#test.delete_customer("17")
+
+customer_list = [
+    ("kdjfsgh","m,fhbg","nvkghdfg"),
+    ("ytvdsdf","ncjfgh","yr87gbf")
+    ]
+#test.add_customer_list(customer_list)
+
+#test.show_all()
+
+test.lookup_customer("first_name", "John")
+
+#test.print_column_names()
